7dba25a510475cb6d5c0500fd532b237fc27fcab
sudhanshudixit03 <sudhanshudixit078@gmail.com>
2026-06-09 10:27:01 +0530

    n

--- Test_code/test_1Valid_login.py
+++ Test_code/test_1Valid_login.py
@@ -3,8 +3,13 @@ def test_valid_login():
       with sync_playwright() as p:
             browser=p.chromium.launch(headless=False)
             page=browser.new_page()
+
+
+
+
+
             page.goto("https://www.saucedemo.com/")             #1st step
-            page.fill("#user-name","standard_user")     #istead of css selectors we can also "use get_by_label"
+            page.fill("#user-name","standard_user")     #instead of css selectors we can also "use get_by_label"
             page.fill("#password","secret_sauce")
             page.click("#login-button")
             # assert page.locator(".title").inner_text() == 'Products'
